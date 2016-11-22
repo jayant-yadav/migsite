77e6075172afc0078be1a7dea61cc6ed43619f59
Jayant Yadav <jayantyadav202@gmail.com>
2016-11-22 23:13:31 +0530

    1st review

--- scripts/app.js
+++ scripts/app.js
@@ -1,11 +1,40 @@
-var mig = angular.module('migApp', ['ngMaterial', 'ngRoute', 'angularCSS']);
+var mig = angular.module('migApp', ['ngMaterial', /*'ui.router'*/ , 'angularCSS']);
+
+//mig.config(function ($stateProvider) {
+//    var createOrder = {
+//        name: 'createOrder',
+//        url: '/Create_order',
+//        templateUrl: './view/createOrder.tmpl.html'
+//            //        controller: 'createOrderController.js'
+//            //        component: 'createOrder'
+//    }
+//
+//
+//    $stateProvider.state(createOrder);
+//});
+
+
+
+
+//mig.component('createOrder', {
+//    //    template: './view/createOrder.tmpl.html',
+//    template: '<h1>HImANSHU</h1>'
+//        //    controller: 'controller/createOrderController.js'
+//})
 
 mig.controller('migController', ['$scope', '$mdDialog', function ($scope, $mdDialog) {
 
+
+    $scope.open = function () {
+        $scope.file = 1;
+    }
+
     $scope.createOrder = function (ev) {
         $mdDialog.show({
+                //                controller: CreateOrder,
                 controller: CreateOrder,
-                templateUrl: 'view/createOrder.tmpl.html',
+                templateUrl: 'createOrder.tmpl.html',
+                //                templateUrl: '.view/createOrder.tmpl.html',
                 parent: angular.element(document.body),
                 targetEvent: ev,
                 clickOutsideToClose: true
@@ -18,6 +47,25 @@ mig.controller('migController', ['$scope', '$mdDialog', function ($scope, $mdDia
     }
 
 
+
+
+
+    $scope.action = function (ev) {
+        $mdDialog.show({
+                //                controller: CreateOrder,
+                controller: CreateOrder,
+                templateUrl: 'action.tmpl.html',
+                //                templateUrl: '.view/createOrder.tmpl.html',
+                parent: angular.element(document.body),
+                targetEvent: ev,
+                clickOutsideToClose: true
+            })
+            .then(function (answer) {
+                //                $scope.status = 'You said the information was "' + answer + '".';
+            }, function () {
+                //                $scope.status = 'You cancelled the dialog.';
+            });
+    }
 
 
 }])
@@ -33,4 +81,6 @@ var CreateOrder = function ($scope, $mdDialog) {
     $scope.answer = function (answer) {
         $mdDialog.hide(answer);
     };
+
+
 }
---
+++ scripts/controller/createOrderController.js
@@ -0,0 +1,36 @@
+mig.controller('createOrderController', ['$scope', '$mdDialog', function ($scope, $mdDialog) {
+
+//    $scope.createOrder = function (ev) {
+        //        $mdDialog.show({
+        //                controller: CreateOrder,
+        //                //                templateUrl: '.view/createOrder.tmpl.html',
+        //                parent: angular.element(document.body),
+        //                targetEvent: ev,
+        //                clickOutsideToClose: true
+        //            })
+        //            .then(function (answer) {
+        //                //                $scope.status = 'You said the information was "' + answer + '".';
+        //            }, function () {
+        //                //                $scope.status = 'You cancelled the dialog.';
+        //            });
+        //    }
+
+
+    console.log("hi");
+
+}])
+
+
+//var CreateOrder = function ($scope, $mdDialog) {
+//    $scope.hide = function () {
+//        $mdDialog.hide();
+//    };
+//    $scope.cancel = function () {
+//        $mdDialog.cancel();
+//    };
+//    $scope.answer = function (answer) {
+//        $mdDialog.hide(answer);
+//    };
+//
+//
+//}
